4bdc912ff6c7bc6736c40f5745775b68caecb120
Rahmatullah Zadran <rahmatullahzadran@outlook.com>
2024-01-20 22:00:17 +0000

    readjusting the clock

--- java.js
+++ java.js
@@ -1,25 +1,40 @@
 console.log("Hello, World!");
 
+
+// Update the clock
 function updateClock() {
     // Get the container element
     var container = document.getElementById('container');
-    container.style.display = 'block';
     container.style.textAlign = 'center';
+    container.style.color = 'black';
+    container.style.fontSize = '40px';
+    container.style.fontWeight = 'bold';
+    container.style.padding = '10px';
+    container.style.margin = '10px';
+    container.style.border = '3px solid blue';
 
     // Get the current time
     var currentTime = new Date();
 
-    // Format the time as HH:MM:SS
     var hours = currentTime.getHours();
     var minutes = currentTime.getMinutes();
     var seconds = currentTime.getSeconds();
+
+    
+
+    // Format the time as HH:MM:SS
+    var year = currentTime.getFullYear();
+    var month = currentTime.getMonth() + 1; 
+    var day = currentTime.getDate();
 
     // Pad single-digit minutes and seconds with a leading zero
     minutes = minutes < 10 ? '0' + minutes : minutes;
     seconds = seconds < 10 ? '0' + seconds : seconds;
 
+    var space = "    ";
+
     // Update the content of the container with the current time
-    container.innerHTML = hours + ':' + minutes + ':' + seconds;
+    container.innerHTML ='Date  ' + year + '-' + month + '-' + day + ' Time  ' + hours + ':' + minutes + ':' + seconds;
 
     // Call the updateClock function again after 1 second
     setTimeout(updateClock, 1000);
@@ -29,3 +44,5 @@ function updateClock() {
   window.onload = function () {
     updateClock();
   };
+
+  
